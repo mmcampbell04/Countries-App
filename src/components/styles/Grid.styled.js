6423c6703080963f6795d064db097382c706e489
Meghan McCall-Campbell <meghan.mccall.campbell@gmail.com>
2022-02-14 18:45:21 +0000

    finsihed header styling

---
+++ src/components/styles/Grid.styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Grid = styled.div`
+  display: grid;
+  grid-template-columns: minmax(1em, 1fr) minmax(0px, 70em) minmax(1em, 1fr);
+`;
--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -3,20 +3,28 @@ import styled from "styled-components";
 export const StyledHeader = styled.header`
   color: ${({ theme }) => theme.textColor};
   background-color: ${({ theme }) => theme.accentColor};
+
+  h1 {
+    font-weight: 800;
+  }
 `;
 
-export const StyledToggle = styled.div`
-  color: ${({ theme }) => theme.textColor};
+export const Nav = styled.nav`
+  grid-column: 2/-2;
+  padding-block: 1em;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 export const CheckboxLabel = styled.label`
-  cursor: pointer;
+  height: 2.5rem;
+  width: 7em;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  height: 2.5rem;
-  //   width: 5rem;
   padding: 5px;
+  cursor: pointer;
 `;
 
 export const Checkbox = styled.input`
