1933e79f2557f8c0ac1368dc454d5213cec62ec2
Meghan McCall-Campbell <meghan.mccall.campbell@gmail.com>
2022-03-23 19:17:04 +0000

    refactored layout styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,10 @@
 // import Toggle from "./Toggle";
 import {
   StyledHeader,
-  Nav,
+  HeaderWrapper,
   Checkbox,
   CheckboxLabel,
 } from "./styles/Header.styled";
-import { Grid } from "./styles/Grid.styled";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoon } from "@fortawesome/free-solid-svg-icons";
 import { faMoon as faMoonLight } from "@fortawesome/free-regular-svg-icons";
@@ -13,25 +12,21 @@ import { faMoon as faMoonLight } from "@fortawesome/free-regular-svg-icons";
 export default function Header({ theme, themeToggler }) {
   return (
     <StyledHeader>
-      <Grid>
-        <Nav>
-          <h1>Where in the world?</h1>
-          <div>
-            <Checkbox
-              type="checkbox"
-              id="themeSwitch"
-              name="theme"
-              onChange={themeToggler}
-            />
-            <CheckboxLabel htmlFor="themeSwitch">
-              <FontAwesomeIcon
-                icon={theme === "light" ? faMoonLight : faMoon}
-              />
-              Dark Mode
-            </CheckboxLabel>
-          </div>
-        </Nav>
-      </Grid>
+      <HeaderWrapper>
+        <h1>Where in the world?</h1>
+        <div className="darkmode toggle">
+          <Checkbox
+            type="checkbox"
+            id="themeSwitch"
+            name="theme"
+            onChange={themeToggler}
+          />
+          <CheckboxLabel htmlFor="themeSwitch">
+            <FontAwesomeIcon icon={theme === "light" ? faMoonLight : faMoon} />
+            Dark Mode
+          </CheckboxLabel>
+        </div>
+      </HeaderWrapper>
     </StyledHeader>
   );
 }
