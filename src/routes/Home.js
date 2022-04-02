f98c3a71eae091225836763475d722d0cda2a26a
Meghan McCall-Campbell <meghan.mccall.campbell@gmail.com>
2022-04-02 18:58:50 +0100

    moved more component logic into context

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,40 +1,14 @@
-import { useState, useContext } from "react";
 import Inputs from "../components/Inputs";
 import CountriesList from "../components/CountriesList";
-// import Pagination from "../components/Pagination";
+import Pagination from "../components/Pagination";
 import { Main } from "../components/styles/Layouts.styled";
-import { Context } from "../Context";
 
 export default function Home() {
-  const { allCountries } = useContext(Context);
-  const [region, setRegion] = useState("");
-  const [searchfield, setSearchfield] = useState("");
-
-  const filteredCountries = allCountries.filter((country) => {
-    return (
-      country.name.toLowerCase().includes(searchfield) &&
-      country.region.toLowerCase().includes(region)
-    );
-  });
-
-  //  search for country
-  function onSearchChange(e) {
-    let userSearch = e.target.value.toLowerCase();
-    setSearchfield(userSearch);
-  }
-
-  function getRegion(continent) {
-    setRegion(continent.toLowerCase());
-  }
-
   return (
     <Main>
-      <Inputs
-        getRegion={getRegion}
-        searchfield={searchfield}
-        onSearchChange={onSearchChange}
-      />
-      <CountriesList filteredCountries={filteredCountries} />
+      <Inputs />
+      <CountriesList />
+      <Pagination />
     </Main>
   );
 }
