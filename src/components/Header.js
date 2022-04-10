6f6342fb5dbc2b0d11a5f28ce78a6c8e138fed12
Meghan McCall-Campbell <meghan.mccall.campbell@gmail.com>
2022-04-10 12:02:48 +0100

    added home link to h1

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-// import Toggle from "./Toggle";
+import { Link } from "react-router-dom";
 import {
   StyledHeader,
   HeaderWrapper,
@@ -12,7 +12,9 @@ export default function Header({ theme, themeToggler }) {
   return (
     <StyledHeader>
       <HeaderWrapper>
-        <h1>Where in the world?</h1>
+        <Link to="/">
+          <h1>Where in the world?</h1>
+        </Link>
         <ThemeButton tabIndex={0} onClick={themeToggler}>
           <FontAwesomeIcon icon={theme === "light" ? faMoonLight : faMoon} />
           Dark Mode
